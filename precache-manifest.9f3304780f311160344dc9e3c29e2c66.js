654f7b2fcf21345fd24016a5e26cddf49579f606
OceaneBligand <obligand@alacrite.fr>
2020-10-01 12:35:02 +0200

    test manifest 1

---
+++ precache-manifest.9f3304780f311160344dc9e3c29e2c66.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bc1fd27c24ac16051b1",
+    "url": "css/1.577691ec.css"
+  },
+  {
+    "revision": "23fcf7091a187b7827a1",
+    "url": "css/4.399b340b.css"
+  },
+  {
+    "revision": "ed2cc931a589ebc4b95f",
+    "url": "css/app.668d16c4.css"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.a45108d3.woff"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "fonts/KFOlCnqEu92Fr1MmEU9fBBc-.cea99d3e.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.865f928c.woff"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "fonts/KFOlCnqEu92Fr1MmWUlfBBc-.2267169e.woff"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "fonts/KFOlCnqEu92Fr1MmYUtfBBc-.bac8362e.woff"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "fonts/KFOmCnqEu92Fr1Mu4mxM.49ae34d4.woff"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "fonts/fa-brands-400.31015e86.woff2"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "fonts/fa-brands-400.ee26f57b.woff"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "fonts/fa-regular-400.8b892b79.woff"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "fonts/fa-regular-400.c5240512.woff2"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "fonts/fa-solid-900.9451d5fe.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "fonts/fa-solid-900.a940d584.woff"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.f2a09334.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.12a47ed5.woff2"
+  },
+  {
+    "revision": "cbceee6585396d5d72f1ca34b63ab053",
+    "url": "img/404.9a86658a.jpg"
+  },
+  {
+    "revision": "1a80b40df25b3b9c0cf475b6d8ae188c",
+    "url": "img/logo2.c5c88392.jpeg"
+  },
+  {
+    "revision": "7dcb705b1876955ec389f6c55a99e1c9",
+    "url": "index.html"
+  },
+  {
+    "revision": "6bc1fd27c24ac16051b1",
+    "url": "js/1.6bc1fd27.js"
+  },
+  {
+    "revision": "79bb81be164449a51dad",
+    "url": "js/2.79bb81be.js"
+  },
+  {
+    "revision": "23fcf7091a187b7827a1",
+    "url": "js/4.23fcf709.js"
+  },
+  {
+    "revision": "b632ce2c4d0a8ee43753",
+    "url": "js/5.b632ce2c.js"
+  },
+  {
+    "revision": "ed2cc931a589ebc4b95f",
+    "url": "js/app.c6a2964f.js"
+  },
+  {
+    "revision": "75978b7c5753b99957e4",
+    "url": "js/vendor.75978b7c.js"
+  },
+  {
+    "revision": "a5a6293f171c1502036be78e0ff7a4fe",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "927a549e04c087dc67920bf38e97ce71",
+    "url": "statics/QrCode.png"
+  },
+  {
+    "revision": "f5cb69823e0771fbf5fe439f2f4fc257",
+    "url": "statics/action.png"
+  },
+  {
+    "revision": "34f64d373df7260f923b226356a5f288",
+    "url": "statics/compass.svg"
+  },
+  {
+    "revision": "e3893474dca17b02b00f201cff75a176",
+    "url": "statics/def.png"
+  },
+  {
+    "revision": "1768f9113edc5ffac9dc09bab336bc80",
+    "url": "statics/defibrillateur.png"
+  },
+  {
+    "revision": "83e3af422eb9286f6c17346fb6e93599",
+    "url": "statics/defibrillateur2.png"
+  },
+  {
+    "revision": "56d8bf42effaa1f167f4f040b3473b47",
+    "url": "statics/evalleyS.png"
+  },
+  {
+    "revision": "6006a6cc8cf5345e48556605307a35d7",
+    "url": "statics/evalleyT.png"
+  },
+  {
+    "revision": "c9920835d2adb9f6ab6a79a27f220a3a",
+    "url": "statics/icons/apple-icon-120x120.png"
+  },
+  {
+    "revision": "9df9e65a1d2c6df3bd9143666b0e8c63",
+    "url": "statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "4300c6c136c06abcd781aa18644dd7a3",
+    "url": "statics/icons/apple-icon-167x167.png"
+  },
+  {
+    "revision": "88dbf3d9e8269f1298486d103e22a57c",
+    "url": "statics/icons/apple-icon-180x180.png"
+  },
+  {
+    "revision": "1be07e62226c8f6a7086f132d9be1642",
+    "url": "statics/icons/favicon-128x128.png"
+  },
+  {
+    "revision": "df5ff7f4728c4ef4b951d46b4136906f",
+    "url": "statics/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "64aa9e88dd6545f64226c1e2230f66cc",
+    "url": "statics/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "d22664397060743b24e8956a5a3fcdec",
+    "url": "statics/icons/favicon-96x96.png"
+  },
+  {
+    "revision": "8b9eb314ff597f6f7c7bf8c7a5f717ec",
+    "url": "statics/icons/favicon.ico"
+  },
+  {
+    "revision": "1be07e62226c8f6a7086f132d9be1642",
+    "url": "statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "a1b423855a450ae7adbd9d2a782e0dfb",
+    "url": "statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "1e4a69ab7f121027ea8dce62f4dfa5ca",
+    "url": "statics/icons/icon-2480x2480.png"
+  },
+  {
+    "revision": "9f33ebc999e51d0dc2975fc573d53a0b",
+    "url": "statics/icons/icon-256x256.png"
+  },
+  {
+    "revision": "a126be0d50ed682c3129e0b8b31f680a",
+    "url": "statics/icons/icon-384x384.png"
+  },
+  {
+    "revision": "02abd24e7c8638e848d96a6bf78d2c27",
+    "url": "statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "0de027adbdce5628a0e47ef6f056f660",
+    "url": "statics/icons/icon.png"
+  },
+  {
+    "revision": "f256ec55be616f32ae486ab7571eb936",
+    "url": "statics/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "de8107bed7f087fb443d8e36855e7467",
+    "url": "statics/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "62675f242900c59a9c5b670bf0ce52eb",
+    "url": "statics/local.png"
+  },
+  {
+    "revision": "e9083ea275a1b4e10e82bf8d80e47923",
+    "url": "statics/location.svg"
+  },
+  {
+    "revision": "2abeae562f98e3f87670f8b7093cea84",
+    "url": "statics/logoEvalleyWhite.png"
+  },
+  {
+    "revision": "37938f58f911ebbc54f5b505ba327145",
+    "url": "statics/map.png"
+  },
+  {
+    "revision": "39a8edccc70f134a8af4816d13dc76a4",
+    "url": "statics/newMap.png"
+  },
+  {
+    "revision": "4fe2c367ce02a36a5273cdd042f97dc9",
+    "url": "statics/quai.png"
+  }
+]);
