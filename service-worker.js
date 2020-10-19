bfe4751f842b5abcbb9184e4bc7dfd15a6a90bfe
OceaneBligand <obligand@alacrite.fr>
2020-10-19 17:16:53 +0200

    fix map size test 7

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.54641d19427e5559a70326d5cb021795.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.d0cb7881ad7e141d45f4028df39db931.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /*
  * This file (which will be your service worker)
