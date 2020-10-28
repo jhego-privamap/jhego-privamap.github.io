f3feb03cfe35009275aa61711fa8347c9b55a89a
OceaneBligand <obligand@alacrite.fr>
2020-10-28 14:57:25 +0100

    no comments

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-importScripts("precache-manifest.f850a1376d11d6152ae72f3816320aaa.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-/*
- * This file (which will be your service worker)
- * is picked up by the build system ONLY if
- * quasar.conf > pwa > workboxPluginMode is set to "InjectManifest"
- */
-
