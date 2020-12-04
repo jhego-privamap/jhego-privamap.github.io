4d0ff665a1d1f0b78e0269982cd50dd999442aa2
OceaneBligand <obligand@alacrite.fr>
2020-12-04 15:17:32 +0100

    url with element id goes directly to directions

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.eee62e1105b3e738b6342268feb356be.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.a43b9f52413433bae77ffdb3cdb87f43.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /*
  * This file (which will be your service worker)
