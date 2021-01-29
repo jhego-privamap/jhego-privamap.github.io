0c68c46abcb61c7b6cb964e51c9bb07a8426a746
OceaneBligand <obligand@privamap.com>
2021-01-29 13:58:40 +0100

    elevator slider only moveable with thumb

--- precache-manifest.61042b699f1af902a0be8e3bbbd47d09.js
+++ precache-manifest.61042b699f1af902a0be8e3bbbd47d09.js
@@ -1,14 +1,14 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0c94b415af7f02145c10",
-    "url": "css/1.84d61c86.css"
+    "revision": "ea67e5bfe1a84645d816",
+    "url": "css/1.d9c9d80b.css"
   },
   {
     "revision": "6407a8187b4e35a935d0",
     "url": "css/4.399b340b.css"
   },
   {
-    "revision": "559acbc56b9906022ec8",
+    "revision": "149bf274bd605468b837",
     "url": "css/app.b34bec91.css"
   },
   {
@@ -84,12 +84,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "img/logo2.c5c88392.jpeg"
   },
   {
-    "revision": "4ba37215311eff639e1f4ddb8e46f1dc",
+    "revision": "605e0e74eee4c837fe63759be2663798",
     "url": "index.html"
   },
   {
-    "revision": "0c94b415af7f02145c10",
-    "url": "js/1.0c94b415.js"
+    "revision": "ea67e5bfe1a84645d816",
+    "url": "js/1.ea67e5bf.js"
   },
   {
     "revision": "79bb81be164449a51dad",
@@ -104,8 +104,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "js/5.b632ce2c.js"
   },
   {
-    "revision": "559acbc56b9906022ec8",
-    "url": "js/app.e0679a0d.js"
+    "revision": "149bf274bd605468b837",
+    "url": "js/app.8849f2b7.js"
   },
   {
     "revision": "5925ca3b46095ad15b61",
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.da13f3bbf40db57cf63139b575677e01.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.61042b699f1af902a0be8e3bbbd47d09.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /*
  * This file (which will be your service worker)
