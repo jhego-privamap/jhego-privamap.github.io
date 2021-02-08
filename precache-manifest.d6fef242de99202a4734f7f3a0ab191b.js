14fe89efa7481de139b1a4f7a58fd3b3087dd638
OceaneBligand <obligand@privamap.com>
2021-02-08 16:39:07 +0100

    no accuracy circle in interior

--- precache-manifest.d6fef242de99202a4734f7f3a0ab191b.js
+++ precache-manifest.d6fef242de99202a4734f7f3a0ab191b.js
@@ -1,14 +1,14 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c959febcb96cf7938e4f",
+    "revision": "e98810c361cfb9452342",
     "url": "css/1.029898f6.css"
   },
   {
-    "revision": "c114be083662bda2ad40",
+    "revision": "9c8637577f8934d00535",
     "url": "css/4.399b340b.css"
   },
   {
-    "revision": "69e03ba92d8f653906d0",
+    "revision": "f02af497a1bb05990315",
     "url": "css/app.b34bec91.css"
   },
   {
@@ -88,28 +88,28 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "img/posCircleGrey.eaa67da4.svg"
   },
   {
-    "revision": "d6af23e39c4d690b522a42469d45a68b",
+    "revision": "f5f6244a738528835f05b5347071874a",
     "url": "index.html"
   },
   {
-    "revision": "c959febcb96cf7938e4f",
-    "url": "js/1.c959febc.js"
+    "revision": "e98810c361cfb9452342",
+    "url": "js/1.e98810c3.js"
   },
   {
     "revision": "79bb81be164449a51dad",
     "url": "js/2.79bb81be.js"
   },
   {
-    "revision": "c114be083662bda2ad40",
-    "url": "js/4.c114be08.js"
+    "revision": "9c8637577f8934d00535",
+    "url": "js/4.9c863757.js"
   },
   {
     "revision": "b632ce2c4d0a8ee43753",
     "url": "js/5.b632ce2c.js"
   },
   {
-    "revision": "69e03ba92d8f653906d0",
-    "url": "js/app.efcf0293.js"
+    "revision": "f02af497a1bb05990315",
+    "url": "js/app.60cca8f1.js"
   },
   {
     "revision": "6a9a10830fa1cebd597f",
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.298934c99f9a22b20dcc06a7b8168f23.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.d6fef242de99202a4734f7f3a0ab191b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /*
  * This file (which will be your service worker)
